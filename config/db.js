03d6fa22e155fb3ad0b19531e852e7c37841d27d
Rockey <rkrockey9690@gmail.com>
2025-09-15 13:09:57 +0530

    commit-next

--- config/db.js
+++ config/db.js
@@ -1,18 +1,25 @@
 import mongoose from "mongoose";
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
-  try {
+  if (cached.conn) return cached.conn;
 
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((mongoose) => {
+      return mongoose;
+    });
   }
 
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 export default connectDB;
